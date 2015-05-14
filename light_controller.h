3beea3a28a117245633f43bff49d8db55d62fdc8
Andrew Pham <andrewpham.vk@gmail.com>
2015-05-14 17:21:57 +0930

    fgdfg

--- light_controller.h
+++ light_controller.h
@@ -3,14 +3,52 @@
 
 #include "glm/glm.hpp"
 
-struct DirectionalLight {
+struct BaseLight {
+	glm::vec3 Colour;
+	float AmbientIntensity;
+	float DiffuseIntensity;
+
+	BaseLight()
+	{
+		Colour = glm::vec3(0.0f, 0.0f, 0.0f);
+		AmbientIntensity = 0.0f;
+		DiffuseIntensity = 0.0f;
+	}
+};
+
+struct DirectionalLight : BaseLight
+{
+	glm::vec3 Direction;
+
+	DirectionalLight()
+	{
+		Direction = glm::vec3(0.0f, 0.0f, 0.0f);
+	}
+};
+
+struct PointLight : BaseLight
+{
+	glm::vec3 Position;
+
+	struct 
+	{
+		float Constant;
+		float Linear;
+		float Exp;
+	} Attenuation;
+
+	PointLight()
+	{
+		Position = glm::vec3();
+	}
+
 
 };
 
 
 class LightController {
 public:
-
+	bool Init();
 	void SetDirectionalLight(const )
 
 private:
